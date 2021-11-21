65bcc675777d3eb2a89807479752c9e402c03892
Jordan Smith <jssmith9876@yahoo.com>
2021-11-20 16:20:26 -0800

    Definitely getting somewhere. Mostly functioning, just want to clean the code up a bit and add some final touches

--- muller.cpp
+++ muller.cpp
@@ -51,28 +51,39 @@ double divided_difference_rec(vector<double> x_vals, vector< vector<double> >& r
     
 }
 
+/*
+ *  Function to calulate the two roots through the quadratic formula.
+ *  Takes in the coefficients a, b, and c from 
+ *      p(x) = ax^2+bx+c
+ *  Returns the roots in a vector. 
+ */
 vector<double> quad_formula(double a, double b, double c) {
-    double x_1, x_2;
+    double r_1, r_2;
 
     double b_sqrd = b * b;
     double discriminant = b_sqrd - 4 * a * c;
 
     if (b < 0) 
-        x_1 = (-b + sqrt(discriminant)) / (2 * a);
+        r_1 = (-b + sqrt(discriminant)) / (2 * a);
     else if (b > 0) 
-        x_1 = (-b - sqrt(discriminant)) / (2 * a);
+        r_1 = (-b - sqrt(discriminant)) / (2 * a);
     else 
-        x_1 = sqrt(c / a);
+        r_1 = sqrt(c / a);
 
-    x_2 = c / (a * x_1);
+    r_2 = c / (a * r_1);
     
     
     vector<double> res;
-    res.push_back(x_1);
-    res.push_back(x_2);
+    res.push_back(r_1);
+    res.push_back(r_2);
 
     return res;
 }
+
+// Absolute value function 
+double _abs(double x) { return (x >= 0) ? x : -x; }
+
+double dist(double x, double y) { return abs(x - y); }
 
 /*
  *  Function to print a 2d vector in a nice format.
@@ -98,53 +109,76 @@ int main() {
         x_vals.push_back(tmp);
     }
 
-    // Fill the table with NaN
-    vector< vector<double> > div_diff_table;
-    for (int i = 0; i < NUM_VALS; i++) {
-        vector<double> tmp;
-        for (int j = 0; j < NUM_VALS; j++) {
-            tmp.push_back(NaN);
-        }
-        div_diff_table.push_back(tmp);
-    }
-
+    // Reverse the values 
     std::reverse(x_vals.begin(), x_vals.end());
 
+    vector< vector<double> > div_diff_table;
 
-    /*
-     *  We want to generate the interpolating polynomial from this data:
-     *      p(x) = a(x-x_0)(x-x_1) + b(x-x_0) + c
-     *  We can calculate a, b, c using the divided difference (Newton Form)
-     */
-    divided_difference_rec(x_vals, div_diff_table, 0, NUM_VALS - 1); // Calculate the divided difference table (stored in `div_diff_table`)
+    for (int iter = 0; iter < 10; iter++) {
+        // Empty the divided difference table
+        for (vector<double> row : div_diff_table) {
+            row.clear();
+        }
+        div_diff_table.clear();
 
 
-    std::cout << "Divided difference table:" << std::endl;
-    prettyPrint(div_diff_table);
+        // Fill the table with NaN
+        for (int i = 0; i < NUM_VALS; i++) {
+            vector<double> tmp;
+            for (int j = 0; j < NUM_VALS; j++) {
+                tmp.push_back(NaN);
+            }
+            div_diff_table.push_back(tmp);
+        }
 
-    // Get the coefficients from the divided difference table
-    vector<double> coeffs;
-    for (int i = 0; i < NUM_VALS; i++) {
-        coeffs.push_back(div_diff_table[i][i]);
+
+        /*
+        *  We want to generate the interpolating polynomial from this data:
+        *      p(x) = a(x-x_0)(x-x_1) + b(x-x_0) + c
+        *  We can calculate a, b, c using the divided difference (Newton Form)
+        */
+        divided_difference_rec(x_vals, div_diff_table, 0, NUM_VALS - 1); // Calculate the divided difference table (stored in `div_diff_table`)
+
+
+        // std::cout << "Divided difference table:" << std::endl;
+        // prettyPrint(div_diff_table);
+
+        // Get the coefficients from the divided difference table
+        vector<double> coeffs;
+        for (int i = 0; i < NUM_VALS; i++) {
+            coeffs.push_back(div_diff_table[i][i]);
+        }
+
+        // Grab the values needed to calculate the polynomial's roots
+        double c_1 = coeffs[2],
+            c_2 = coeffs[1],
+            c_3 = coeffs[0];
+        double x_1 = x_vals[1],
+            x_2 = x_vals[0];
+        double a = c_1,
+            b = (-c_1 * x_1 - c_1 * x_2 + c_2),
+            c = (c_1 * x_1 * x_2 - c_2 * x_2 + c_3);
+
+        // Calculate the roots of the interpolating polynomial
+        //std::cout << "Roots of interpolating polynomial" << std::endl;
+        vector<double> roots = quad_formula(a, b, c);
+
+        // for (double& root : roots) {
+        //     std::cout << root << " ";
+        // }
+        // std::cout << std::endl;
+
+        // Calculate the new x value from the roots
+        double new_x = (dist(x_2, roots[0]) < dist(x_2, roots[1])) ? roots[0] : roots[1];
+        
+        //std::cout << "New x value is: " << new_x << std::endl;
+
+        // Make the new vector with the new value
+        x_vals.insert(x_vals.begin(), new_x);
+        x_vals.pop_back();
+
+        std::cout << "x_" << iter + 1 << " = " << new_x << std::endl;
     }
-    double c_1 = coeffs[0],
-           c_2 = coeffs[1],
-           c_3 = coeffs[2];
-
-    double x_1 = x_vals[1],
-           x_2 = x_vals[2];
-
-    double a = c_1,
-           b = (-c_1 * x_1 - c_1 * x_2 + c_2),
-           c = (c_1 * x_1 * x_2 - c_2 * x_2 + c_3);
-
-    std::cout << "Roots of interpolating polynomial" << std::endl;
-    vector<double> roots = quad_formula(a, b, c);
-
-    for (double& root : roots) {
-        std::cout << root << " ";
-    }
-    std::cout << std::endl;
-
+    
     return 0;
 }
