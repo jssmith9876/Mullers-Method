aafc3d8425ff6a8a108ebef40cb672f465d9bdb7
Jordan Smith <jssmith9876@yahoo.com>
2021-11-21 12:54:19 -0800

    Added more comments and documentation and added checks for complex roots

--- muller.cpp
+++ muller.cpp
@@ -1,19 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <limits>
-#include <algorithm>
-#include <math.h>
-#define NUM_VALS 3
-
-using std::vector; // So we don't have to write std::vector every time
-
 /*
+ * Muller's Method
+ *  author: Jordan Smith
+ * 
+ * Program Purpose:
+ *  To approximate a root of a function f(x) using Muller's Method
+ *
  * Steps:
  *  1. Get x_0, x_1, and x_2 from the user
  *  2. Generate the polynomial p(x) that interpolates those three points
  *  3. Calculate the roots of p(x) and take x_3 to be the root that is closest to x_2
  *  4. Repeat this process with x_1, x_2, and x_3
+ * 
+ * The program will iterate this method 10 times, or until the interpolating polynomial it uses
+ *  to approximate the function has only complex roots. 
+ * 
+ * To run the program:
+ *  1) run `g++ -std=c++14 muller.cpp`
+ *  2) run `./a.out`
  */
+
+#include <iostream>     // For input/output
+#include <vector>       // Easier to manage arrays
+#include <limits>       // To generate NaN for the divided difference table
+#include <algorithm>    // For reversing our vector
+#include <stdlib.h>     // So we can use EXIT_FAILURE is something goes wrong
+#include <math.h>       // For the sqrt function 
+#include <iomanip>      // So we can set the precision of the numbers we output
+#define NUM_VALS 3
+#define DEBUG 0 
+
+using std::vector;  // So we don't have to write std::vector every time
+
 
 // Global NaN value to use in the empty values of our table
 const double NaN = std::numeric_limits<double>::quiet_NaN();
@@ -58,31 +75,28 @@ double divided_difference_rec(vector<double> x_vals, vector< vector<double> >& r
  *  Returns the roots in a vector. 
  */
 vector<double> quad_formula(double a, double b, double c) {
+    // Define the variables we need to use
     double r_1, r_2;
-
     double b_sqrd = b * b;
     double discriminant = b_sqrd - 4 * a * c;
 
-    if (b < 0) 
-        r_1 = (-b + sqrt(discriminant)) / (2 * a);
-    else if (b > 0) 
-        r_1 = (-b - sqrt(discriminant)) / (2 * a);
-    else 
-        r_1 = sqrt(c / a);
+    // The sign of b to make calculations easier
+    int sgn_b = (b > 0) ? 1 : -1;
 
+    // If we get a negative discriminant, we have complex roots (BAD NEWS)
+    if (discriminant < 0) exit(EXIT_FAILURE);
+
+    // Calculate the roots 
+    r_1 = (-b - sgn_b * sqrt(discriminant)) / (2 * a);
     r_2 = c / (a * r_1);
     
-    
-    vector<double> res;
-    res.push_back(r_1);
-    res.push_back(r_2);
-
+    // Return the roots as a vector
+    vector<double> res {r_1, r_2};
     return res;
 }
 
-// Absolute value function 
+// Helper functions to decide the next x value 
 double _abs(double x) { return (x >= 0) ? x : -x; }
-
 double dist(double x, double y) { return abs(x - y); }
 
 /*
@@ -97,6 +111,7 @@ void prettyPrint(vector< vector<double> > vals) {
         std::cout << std::endl;
     }
 }
+
 
 int main() {
     vector<double> x_vals;
@@ -114,22 +129,28 @@ int main() {
 
     vector< vector<double> > div_diff_table;
 
+    // Set the output precision 
+    std::cout << std::fixed << std::setprecision(8);
+
     for (int iter = 0; iter < 10; iter++) {
-        // Empty the divided difference table
-        for (vector<double> row : div_diff_table) {
-            row.clear();
-        }
-        div_diff_table.clear();
 
-
-        // Fill the table with NaN
-        for (int i = 0; i < NUM_VALS; i++) {
-            vector<double> tmp;
-            for (int j = 0; j < NUM_VALS; j++) {
-                tmp.push_back(NaN);
+        // Reset the divided difference table
+        if (div_diff_table.empty()) {
+            // Fill the table with NaN
+            for (int i = 0; i < NUM_VALS; i++) {
+                vector<double> tmp;
+                for (int j = 0; j < NUM_VALS; j++) {
+                    tmp.push_back(NaN);
+                }
+                div_diff_table.push_back(tmp);
             }
-            div_diff_table.push_back(tmp);
+        } else {
+            // Set the table full of NaN
+            for (int i = 0; i < NUM_VALS; i++) 
+                for (int j = 0; j < NUM_VALS; j++) 
+                    div_diff_table[i][j] = NaN;
         }
+
 
 
         /*
@@ -139,9 +160,10 @@ int main() {
         */
         divided_difference_rec(x_vals, div_diff_table, 0, NUM_VALS - 1); // Calculate the divided difference table (stored in `div_diff_table`)
 
-
-        // std::cout << "Divided difference table:" << std::endl;
-        // prettyPrint(div_diff_table);
+#if DEBUG
+        std::cout << "Divided difference table:" << std::endl;
+        prettyPrint(div_diff_table);
+#endif
 
         // Get the coefficients from the divided difference table
         vector<double> coeffs;
@@ -155,18 +177,22 @@ int main() {
             c_3 = coeffs[0];
         double x_1 = x_vals[1],
             x_2 = x_vals[0];
+
+        // Calculate the coeffs of ax^2+bx+c
         double a = c_1,
             b = (-c_1 * x_1 - c_1 * x_2 + c_2),
             c = (c_1 * x_1 * x_2 - c_2 * x_2 + c_3);
 
         // Calculate the roots of the interpolating polynomial
-        //std::cout << "Roots of interpolating polynomial" << std::endl;
         vector<double> roots = quad_formula(a, b, c);
 
-        // for (double& root : roots) {
-        //     std::cout << root << " ";
-        // }
-        // std::cout << std::endl;
+#if DEBUG
+        std::cout << "Roots of interpolating polynomial" << std::endl;
+        for (double& root : roots) {
+            std::cout << root << " ";
+        }
+        std::cout << std::endl;
+#endif
 
         // Calculate the new x value from the roots
         double new_x = (dist(x_2, roots[0]) < dist(x_2, roots[1])) ? roots[0] : roots[1];
@@ -178,6 +204,10 @@ int main() {
         x_vals.pop_back();
 
         std::cout << "x_" << iter + 1 << " = " << new_x << std::endl;
+
+#if DEBUG
+        std::cout << "\n====================================\n\n";
+#endif
     }
     
     return 0;
